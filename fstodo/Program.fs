86a96510672c29d4063bfdc25fe58211db781250
Alexander Nyquist <nyquist.alexander@gmail.com>
2015-08-20 21:09:12 +0200

    Refactorings.

--- fstodo/Program.fs
+++ fstodo/Program.fs
@@ -1,23 +1,21 @@
 ﻿open System;
 open System.IO;
 
-// Remove Nth item from file
-let remove path num =
-    let n = Int32.Parse(num) - 1
+let read path = File.ReadAllLines path |> Array.toList
 
-    File.ReadAllLines(path)
-    |> Array.toList
-    |> List.mapi (fun i line -> (i, line)) // Create a tuple with (index, line)
-    |> List.filter (fun x -> fst x <> n) // Filter Nth item (first tuple member)
-    |> List.map (fun x -> snd x) // Take line (second tuple member)
+// Remove Nth item from file
+let remove (path, num) =
+    read path
+    |> List.mapi (fun i line -> (i <> num, line)) // Create a tuple with (shouldkeep, line)
+    |> List.filter fst // Filter Nth item (first tuple member)
+    |> List.map snd // Take line (second tuple member)
     |> List.toArray
     |> fun(lines) -> File.WriteAllLines(path, lines)
     |> ignore
 
 // Show todo list
 let display path =
-    File.ReadAllLines(path)
-    |> Array.toList
+    read path
     |> List.mapi (fun i line -> (i+1, line))
     |> List.iter (fun (i, line) -> printf "%d. %s\n" i line)
     |> ignore
@@ -34,11 +32,9 @@ let main argv =
     let path = Environment.GetEnvironmentVariable("TODO_FILE", EnvironmentVariableTarget.User)
 
     match argv with
-        | [||] -> () // No arguments, skip
-        | _ -> match argv.[0] with
-               | "done" -> remove path argv.[1] // How the fuck do i call Int32.Parse argv.[1] and call remove (like remove (path, Int32.Parse(argv.[1])))?
-               | _ -> add path argv
+        | [||] -> ()
+        | [|"done"; num|] -> remove(path, Int32.Parse(num) - 1)
+        | _ -> add path argv
 
     display path
-
-    0 // return an integer exit code
+    0
